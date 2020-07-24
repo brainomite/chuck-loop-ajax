aee1e79a5d04f2331190d0f39f88d0312c5a9d21
Aaron Young <brainomite@gmail.com>
2020-07-24 08:12:15 -0400

    newest and latest server

---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+app.get("/", function (_, res) {
+  res.sendFile(path.join(__dirname, "assets", "index.html"));
+});
+
+app.get("/robots", (_, res) => {
+  res.statusCode = 200;
+  if (randomIntFromInterval(1, 3) === 3) {
+    res.send({ done: true });
+  } else {
+    const robot = `https://robohash.org/${Date.now()}`;
+    res.send({ robot });
+  }
+});
+
+const port = process.env.PORT || 4000;
+const onListen = () => console.log("Running on port " + port);
+app.listen(port, onListen);
